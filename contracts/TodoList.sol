bd3fea10b3409c4d4c3430593b2d597bc7cc0bb6
ameliemw <48097868 ameliemw@users.noreply.github.com>
2025-06-03 12:42:49 +0200

    Setup 2.0

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -3,6 +3,25 @@ pragma solidity ^0.5.0;
 contract TodoList {
     uint public taskCount = 0; //Represent the state of the variable on the blockchain
 
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks; //get the whole content of the task using the id
+    
+    constructor() public {
+        createTask("Check out fuzzsjakk.no"); //Make a default task
+    }
+    
+    function createTask(string memory _content) public {
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount, _content, false); //Make a new task in the dct/mapping
+    }
+
+
+
 }
 
 
